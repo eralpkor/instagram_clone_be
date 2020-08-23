1c3b80a6cd6e1b33e309de3075e8ab58d3675a21
eralpkor <eralp.ny@gmail.com>
2020-08-23 11:06:35 -0400

    Add npm packages

--- src/api/server.js
+++ src/api/server.js
@@ -2,11 +2,18 @@ const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
 
-const main = require('')
+// const main = require('')
 const server = express();
 
 server.use(helmet());
 server.use(cors());
 server.use(express.json());
 
-server.use('/', main);
+// server.use('/', main);
+
+server.get('/', (req, res) => {
+  res.send("<h2>Let's cook something! 🌽🥕 😄</h2>");
+});
+
+
+module.exports = server;
